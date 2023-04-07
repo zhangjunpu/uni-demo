6ca6502b0839c9cbf846a7689e3bb2a7a813d90d
Junpu <junpu0311@163.com>
2023-04-07 23:34:33 +0800

    update anim page.

---
+++ src/components/anim-card/index.vue
@@ -0,0 +1,200 @@
+<template>
+  <view class="anim-card">
+    <view
+      v-for="(item, index) in list"
+      :key="item.name"
+      :class="['item', targetItemCls(index), targetAnimCls(index)]"
+      :style="{ background: item.bg }"
+      @click="handleItem(index)"
+      @touchstart="touchStart"
+      @touchend="touchEnd">
+      {{ item.name }}
+    </view>
+  </view>
+</template>
+
+<script>
+export default {
+  name: 'anim-card',
+  props: {
+    list: {
+      type: Array,
+      default: () => {
+        return [
+          { name: 'item1', bg: 'red' },
+          { name: 'item2', bg: 'orange' },
+          { name: 'item3', bg: 'yellow' },
+          { name: 'item4', bg: 'green' },
+          { name: 'item5', bg: 'skyblue' },
+          { name: 'item6', bg: 'blue' },
+          { name: 'item7', bg: 'purple' },
+          { name: 'item8', bg: 'pink' },
+        ];
+      },
+    },
+    touchSlop: {
+      type: Number,
+      default: 20,
+    },
+  },
+  data() {
+    return {
+      animCls: ['item0', 'item1', 'item2', 'item3'],
+      curIndex: 0,
+      direction: 0, // 0 none，1 正向，2 反向
+    };
+  },
+  methods: {
+    targetItemCls(index) {
+      const i = Math.min(this.targetIndex(index), 3);
+      return this.animCls[i];
+    },
+    targetAnimCls(index) {
+      const size = this.list.length;
+      const i = this.targetIndex(index);
+      if (this.direction === 1 && i === size - 1) {
+        return 'anim-forward';
+      } else if (this.direction === 2 && i === 0) {
+        return 'anim-backward';
+      }
+      return '';
+    },
+    targetIndex(index) {
+      const size = this.list.length;
+      return (index - this.curIndex + size) % size;
+    },
+    handleItem(index) {
+      console.log('click', index);
+    },
+    handlePrev() {
+      this.direction = 2;
+      let index = this.curIndex - 1;
+      if (index < 0) index = this.list.length - 1;
+      this.curIndex = index;
+    },
+    handleNext() {
+      this.direction = 1;
+      let index = this.curIndex + 1;
+      if (index >= this.list.length) index = 0;
+      this.curIndex = index;
+    },
+    touchStart(e) {
+      const { clientX, clientY } = e.changedTouches && e.changedTouches[0];
+      this.downX = clientX;
+      this.downY = clientY;
+    },
+    touchEnd(e) {
+      const { clientX, clientY } = e.changedTouches && e.changedTouches[0];
+      const offsetX = clientX - this.downX;
+      const offsetY = clientY - this.downY;
+      let res = Math.abs(offsetX) > Math.abs(offsetY) ? offsetX : offsetY;
+      if (Math.abs(res) <= 20) return;
+      res >= 0 ? this.handleNext() : this.handlePrev();
+    },
+  },
+  mounted() {
+    console.log(this);
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.anim-card {
+  width: 100%;
+  height: 348rpx;
+  position: relative;
+
+  .item {
+    position: absolute;
+    width: 694rpx;
+    height: 288rpx;
+    line-height: 288rpx;
+    border-radius: 30rpx;
+    margin: 40rpx 0 20rpx 16rpx;
+    text-align: center;
+    box-shadow: 0 0 8rpx #bbb;
+    perspective: 500rpx;
+    transition: all 300ms ease;
+  }
+}
+
+.item0 {
+  transform: translate3d(0, 0, 0);
+  z-index: 3;
+  opacity: 1;
+}
+
+.item1 {
+  transform: translate3d(10rpx, -10rpx, -10rpx);
+  z-index: 2;
+  opacity: 1;
+}
+
+.item2 {
+  transform: translate3d(20rpx, -20rpx, -20rpx);
+  z-index: 1;
+  opacity: 1;
+}
+
+.item3 {
+  transform: translate3d(20rpx, -20rpx, -20rpx);
+  z-index: 0;
+  opacity: 0;
+}
+
+.anim-forward {
+  transition: none;
+  animation: anim-forward 300ms linear 1 forwards;
+}
+
+.anim-backward {
+  transition: none;
+  animation: anim-backward 300ms linear 1 forwards;
+}
+
+@keyframes anim-forward {
+  from {
+    transform: translate3d(0, 0, 0);
+    z-index: 4;
+    opacity: 1;
+  }
+
+  70% {
+    transform: translate3d(0, 200rpx, 0) rotate(-10deg);
+    z-index: 4;
+    opacity: 1;
+  }
+
+  to {
+    transform: translate3d(20rpx, -20rpx, -20rpx);
+    z-index: 0;
+    opacity: 0;
+  }
+}
+
+@keyframes anim-backward {
+  from {
+    transform: translate3d(20rpx, -20rpx, -20rpx);
+    z-index: 0;
+    opacity: 0;
+  }
+
+  50% {
+    transform: translate3d(0, 200rpx, 0);
+    z-index: 0;
+    opacity: 1;
+  }
+
+  51% {
+    transform: translate3d(0, 200rpx, 0);
+    z-index: 3;
+    opacity: 1;
+  }
+
+  to {
+    transform: translate3d(0, 0, 0);
+    z-index: 3;
+    opacity: 1;
+  }
+}
+</style>
--- src/pages/animate/index.vue
+++ src/pages/animate/index.vue
@@ -1,198 +1,27 @@
 <template>
-  <view class="animate">
-    <view class="list">
-      <view
-        v-for="(item, index) in list"
-        :key="item.name"
-        :class="['item', targetItemCls(index), targetAnimCls(index)]"
-        :style="'background:' + item.bg"
-        @click="handleItem(index)"
-      >
-        {{ item.name }}
-      </view>
-    </view>
-
-    <view class="ctrl">
-      <button type="primary" class="btn prev" @click="handlePrev">Prev</button>
-      <button type="primary" class="btn next" @click="handleNext">Next</button>
-    </view>
+  <view class="container">
+    <AnimCard />
   </view>
 </template>
 
 <script>
+import AnimCard from '@/components/anim-card';
 export default {
+  components: {
+    AnimCard,
+  },
   data() {
-    return {
-      list: [
-        { name: "item1", bg: "red" },
-        { name: "item2", bg: "orange" },
-        { name: "item3", bg: "yellow" },
-        { name: "item4", bg: "green" },
-        { name: "item5", bg: "skyblue" },
-        { name: "item6", bg: "blue" },
-        { name: "item7", bg: "purple" },
-        { name: "item8", bg: "pink" },
-      ],
-      animCls: ["item0", "item1", "item2", "item3"],
-      curIndex: 0,
-      direction: 0, // 0 none，1 正向，2 反向
-    };
+    return {};
   },
   onLoad(options) {
     console.log(this);
   },
-  methods: {
-    targetItemCls(index) {
-      const i = Math.min(this.targetIndex(index), 3);
-      return this.animCls[i];
-    },
-    targetAnimCls(index) {
-      const size = this.list.length;
-      const i = this.targetIndex(index);
-      if (this.direction === 1 && i === size - 1) {
-        return "anim-forward";
-      } else if (this.direction === 2 && i === 0) {
-        return "anim-backward";
-      }
-      return "";
-    },
-    targetIndex(index) {
-      const size = this.list.length;
-      return (index - this.curIndex + size) % size;
-    },
-    handleItem(index) {
-      console.log("click", index);
-    },
-    handlePrev() {
-      this.direction = 2;
-      let index = this.curIndex - 1;
-      if (index < 0) index = this.list.length - 1;
-      this.curIndex = index;
-    },
-    handleNext() {
-      this.direction = 1;
-      let index = this.curIndex + 1;
-      if (index >= this.list.length) index = 0;
-      this.curIndex = index;
-    },
-  },
+  methods: {},
 };
 </script>
 
 <style scoped lang="scss">
 page {
   background-color: #f2f2f2;
-}
-.animate {
-  .ctrl {
-    position: absolute;
-    left: 0;
-    bottom: 0;
-    right: 0;
-    display: flex;
-    justify-content: center;
-
-    .btn {
-      width: 40%;
-      margin: 20rpx;
-    }
-  }
-
-  .list {
-    position: relative;
-
-    .item {
-      position: absolute;
-      width: 694rpx;
-      height: 288rpx;
-      line-height: 288rpx;
-      border-radius: 30rpx;
-      margin: 40rpx 16rpx;
-      text-align: center;
-      box-shadow: 0 0 8rpx #bbb;
-      perspective: 500rpx;
-      transition: all 300ms ease;
-    }
-  }
-}
-
-.item0 {
-  transform: translate3d(0, 0, 0);
-  z-index: 3;
-  opacity: 1;
-}
-
-.item1 {
-  transform: translate3d(10rpx, -10rpx, -10rpx);
-  z-index: 2;
-  opacity: 1;
-}
-
-.item2 {
-  transform: translate3d(20rpx, -20rpx, -20rpx);
-  z-index: 1;
-  opacity: 1;
-}
-
-.item3 {
-  transform: translate3d(20rpx, -20rpx, -20rpx);
-  z-index: 0;
-  opacity: 0;
-}
-
-.anim-forward {
-  transition: none;
-  animation: anim-forward 300ms linear 1 forwards;
-}
-
-.anim-backward {
-  transition: none;
-  animation: anim-backward 300ms linear 1 forwards;
-}
-
-@keyframes anim-forward {
-  from {
-    transform: translate3d(0, 0, 0);
-    z-index: 4;
-    opacity: 1;
-  }
-
-  70% {
-    transform: translate3d(0, 200rpx, 0) rotate(-10deg);
-    z-index: 4;
-    opacity: 1;
-  }
-
-  to {
-    transform: translate3d(20rpx, -20rpx, -20rpx);
-    z-index: 0;
-    opacity: 0;
-  }
-}
-
-@keyframes anim-backward {
-  from {
-    transform: translate3d(20rpx, -20rpx, -20rpx);
-    z-index: 0;
-    opacity: 0;
-  }
-
-  50% {
-    transform: translate3d(0, 200rpx, 0);
-    z-index: 0;
-    opacity: 1;
-  }
-
-  51% {
-    transform: translate3d(0, 200rpx, 0);
-    z-index: 3;
-    opacity: 1;
-  }
-
-  to {
-    transform: translate3d(0, 0, 0);
-    z-index: 3;
-    opacity: 1;
-  }
 }
 </style>
